3915b01904cdb91736ebd45d5dd1895b39fe45f7
Markus Tukiainen <p67556@edu.sasky.fi>
2024-05-08 16:37:12 +0300

    deploy.php-tiedoston host päivitetty

--- deploy.php
+++ deploy.php
@@ -21,9 +21,10 @@ set('writable_dirs', []);
 
 
 // Hosts
-
 host('neutroni.hayo.fi')
-    ->set('deploy_path', '~/public_html/{{application}}');    
+    ->user('mtukiain')
+    ->set('deploy_path', '~/public_html/{{application}}');
+    
     
 
 // Tasks
